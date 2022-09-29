64e5b96fc2eae5c0080f963423e5b95b5f21e1f0
James Jabar <jabar.james.jj@gmail.com>
2022-09-29 19:02:40 -0400

    final push

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ const questions = [
         name:"installation",
         message:"what was the installation of your repo?",
     },
+   { type:"input",
+    name:"usage",
+    message:"provide instructions and examples for use"
+},
+        {
+        type:"input",
+        name:"credits",
+        message:"List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.",
+
+        },
+        {
+            type:"input"
+            name:"license"
+            message:"The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/)."
+        }
 ];
 
 // TODO: Create a function to write README file
